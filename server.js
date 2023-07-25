1d30c3bbbeb17cd68594ad07be314494910c9a55
Hamed <ostovari.eng@gmail.com>
2023-07-25 11:12:16 +0400

    added color to services

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const defaulColorObject = require('./colors');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 let customHtml = '';
+let colors = defaulColorObject;
 
 app.get('/config', (req, res) => {
   res.status(200).json({
@@ -20,6 +22,53 @@ app.post('/config', (req, res) => {
   res.status(200).json({ status: 'ok', message: 'Successfuly added' });
 });
 
-app.listen(3001, () => {
-  console.log('app is listening to port 3000');
+function generateCSS(params) {
+  let defaultCSS = `
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f0f0f0;
+    }
+
+    h1 {
+      color: #007bff;
+    }
+  `;
+
+  Object.keys(colors).forEach((key) => {
+    defaultCSS += `
+      .text-${key}{
+        color:${colors[key]}
+      }
+      .bg-${key}{
+        background-color:${colors[key]}
+      }
+      `;
+  });
+}
+
+app.post('/api/css', (req, res) => {
+  const { body } = req;
+  const { name, value } = body;
+  colors = {
+    ...colors,
+    name: {
+      ...colors[name],
+      value,
+    },
+  };
+  const cssContent = generateCSS(queryParams);
+  res.status(200).send('config is saved');
+});
+
+app.get('/api/css', (req, res) => {
+  const queryParams = req.query;
+  const cssContent = generateCSS(queryParams);
+
+  res.setHeader('Content-Type', 'text/css');
+  res.setHeader('Content-Disposition', 'attachment; filename="styles.css"');
+  res.status(200).send(cssContent);
+});
+
+app.listen(8000, () => {
+  console.log('app is listening to port 3002');
 });
